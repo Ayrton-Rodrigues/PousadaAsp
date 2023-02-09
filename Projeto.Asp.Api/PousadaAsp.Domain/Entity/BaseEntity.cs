f375621fde6d5c7c88aca0d6e037421e3bd3b633
Ayrton Rodrigues <92091259+Ayrton-Rodrigues@users.noreply.github.com>
2023-02-08 22:38:36 -0300

    feat: Classe base para outras entidades

--- Projeto.Asp.Api/PousadaAsp.Domain/Entity/BaseEntity.cs
+++ Projeto.Asp.Api/PousadaAsp.Domain/Entity/BaseEntity.cs
@@ -1,6 +1,14 @@
-﻿namespace Projeto.Asp.Api.PousadaAsp.Domain.Entity
+﻿using System;
+
+namespace Projeto.Asp.Api.PousadaAsp.Domain.Entity
 {
-    public class BaseEntity
+    public abstract class BaseEntity
     {
+        protected BaseEntity()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; set; }
     }
 }
