4009080a1032219592bc5c54b9b18d4cba4dd1c5
Ayrton Rodrigues <92091259+Ayrton-Rodrigues@users.noreply.github.com>
2023-02-08 22:39:41 -0300

    feat: Classe de usuario

--- Projeto.Asp.Api/PousadaAsp.Domain/Entity/User.cs
+++ Projeto.Asp.Api/PousadaAsp.Domain/Entity/User.cs
@@ -1,6 +1,25 @@
-﻿namespace Projeto.Asp.Api.PousadaAsp.Domain.Entity
+﻿
+using System;
+
+namespace Projeto.Asp.Api.PousadaAsp.Domain.Entity
 {
-    public class User
+    public class User : BaseEntity
     {
+
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Cpf { get; set; }
+        public string Password { get; set; }
+        public int Roles { get; set; }
+
+        public DateTime Data_Nascimento { get; set; }
+
+        public bool Ativo { get; set; }
+
+
+    }   
+
+    
     }
-}
+
+   
