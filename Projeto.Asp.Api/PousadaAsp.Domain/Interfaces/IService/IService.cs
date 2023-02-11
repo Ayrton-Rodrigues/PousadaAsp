2c2b78035fb451a8191670fd2b526ed8453cc2e4
Ayrton Rodrigues <92091259+Ayrton-Rodrigues@users.noreply.github.com>
2023-02-10 21:34:26 -0300

    fix: ajustes nos tipo

--- Projeto.Asp.Api/PousadaAsp.Domain/Interfaces/IService/IService.cs
+++ Projeto.Asp.Api/PousadaAsp.Domain/Interfaces/IService/IService.cs
@@ -1,6 +1,15 @@
-﻿namespace Projeto.Asp.Api.PousadaAsp.Domain.Interfaces.IService
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Projeto.Asp.Api.PousadaAsp.Domain.Interfaces.IService
 {
-    public class IService
+    public interface IService<TEntity, K> : IDisposable
     {
+        Task<IEnumerable<TEntity>> GetAll();
+        Task<TEntity> GetById(K id);
+        Task Add(TEntity entity);
+        Task Update(TEntity entity);
+        Task Delete(TEntity entity);
     }
 }
--- Projeto.Asp.Api/PousadaAsp.Domain/Interfaces/IService/IUserService.cs
+++ Projeto.Asp.Api/PousadaAsp.Domain/Interfaces/IService/IUserService.cs
@@ -6,33 +6,7 @@ using System.Threading.Tasks;
 
 namespace Projeto.Asp.Api.PousadaAsp.Domain.Interfaces.IService
 {
-    public class IUserService : IService<UserViewModel, Guid>
+    public interface IUserService : IService<UserViewModel, Guid>
     {
-        private readonly IUserRepository _userRepository;
-        private readonly IMapper _mapper;
-        public Task<IEnumerable<UserViewModel>> GetAll()
-        {
-            throw new NotImplementedException();
-        }
-        public Task<UserViewModel> GetById(Guid id)
-        {
-            throw new NotImplementedException();
-        }
-        public Task Add(UserViewModel entity)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task Update(UserViewModel entity)
-        {
-            throw new NotImplementedException();
-        }
-        public Task Delete(UserViewModel entity)
-        {
-            throw new NotImplementedException();
-        }
-
-
-
     }
 }
