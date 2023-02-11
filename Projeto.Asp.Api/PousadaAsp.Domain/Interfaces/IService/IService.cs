a1adcc78fefef1c9f3208dff95c994dbcf1581e3
Ayrton Rodrigues <92091259+Ayrton-Rodrigues@users.noreply.github.com>
2023-02-10 21:09:52 -0300

    feat: interface para os usuarios

---
+++ Projeto.Asp.Api/PousadaAsp.Domain/Interfaces/IService/IService.cs
@@ -0,0 +1,6 @@
+﻿namespace Projeto.Asp.Api.PousadaAsp.Domain.Interfaces.IService
+{
+    public class IService
+    {
+    }
+}
---
+++ Projeto.Asp.Api/PousadaAsp.Domain/Interfaces/IService/IUserService.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using Projeto.Asp.Api.PousadaAsp.Domain.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Projeto.Asp.Api.PousadaAsp.Domain.Interfaces.IService
+{
+    public class IUserService : IService<UserViewModel, Guid>
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
+        public Task<IEnumerable<UserViewModel>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+        public Task<UserViewModel> GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+        public Task Add(UserViewModel entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Update(UserViewModel entity)
+        {
+            throw new NotImplementedException();
+        }
+        public Task Delete(UserViewModel entity)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+    }
+}
