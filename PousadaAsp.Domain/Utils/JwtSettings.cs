04d2b87513c5069ea972663fdfc885fab04b8e81
Ayrton Rodrigues <92091259+Ayrton-Rodrigues@users.noreply.github.com>
2023-02-26 00:25:59 -0300

    feat: Adicionado funcionalidade para tratar erros e notificar no response para o client

---
+++ PousadaAsp.Domain/Utils/JwtSettings.cs
@@ -0,0 +1,13 @@
+﻿namespace PousadaAsp.Domain.Utils
+{
+    public class JwtSettings
+    {
+        public string Secret { get; set; }
+
+        public int ExpiracaoHoras { get; set; }
+
+        public string Emissor { get; set; }
+        
+        public string ValidoEm { get; set; }
+    }
+}
